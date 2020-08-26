3c6ef9fe646360a9f6883f0744c9237a42436b1d
Perfspec <lewisrwebb@gmail.com>
2020-08-26 18:05:49 +0100

    v0.5.0

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,12 +2,11 @@ use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse, Erro
 use serde::{Serialize, Deserialize};
 use futures::future::{ready, Ready};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct User {
     user_id: u32
 }
 
-// Responder
 impl Responder for User {
     type Error = Error;
     type Future = Ready<Result<HttpResponse, Error>>;
@@ -27,21 +26,47 @@ struct UriParams {
     user_id: u32
 }
 
+#[derive(Serialize)]
+struct UserId {
+    user_id: u32
+}
+
+impl Responder for UserId {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+
+        // Create response and set content type
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)))
+    }
+}
+
+// Begin Handlers
+
 async fn get_user(path: web::Path<UriParams>) -> User {
 	User { user_id: path.user_id }
 }
 
+async fn create_user(body: web::Json<User>) -> UserId {
+	UserId { user_id: body.user_id }
+}
+
+// End Handlers
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-	let host = "127.0.0.1";
-	let port = "8088";
-	let ip_address = format!("{}:{}", host, port);
+	let ip_address = "127.0.0.1:8088";
 	
     HttpServer::new(|| {
         App::new()
 			.service(
-				web::scope("/users")
-					.route("/{user_id}", web::get().to(get_user)))
+				web::scope("/api")
+					.route("/users", web::post().to(create_user))
+					.route("/users/{user_id}", web::get().to(get_user)))
 	})
     .bind(ip_address)?
     .run()
